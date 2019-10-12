9ff14511426d3b65b5894d62245c9bef52c7e8dc
FilipAdrian <afilip@ms3-inc.com>
2019-10-12 12:08:19 +0300

    create searchJsonKey function

--- networkProg_Project_1/src/main/java/HttpClient.java
+++ networkProg_Project_1/src/main/java/HttpClient.java
@@ -1,16 +1,27 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.protocol.RequestUserAgent;
 import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.*;
-import java.util.Properties;
+import java.util.*;
+import java.util.jar.JarException;
 
 public class HttpClient {
     static Logger logger = Logger.getLogger (HttpClient.class.getName ( ));
     private String baseUri;
+    private String accessToken;
+    private static List <String> list = new ArrayList <> ( );
+
 
     HttpClient() {
         try {
@@ -28,10 +39,56 @@ public class HttpClient {
     public void get(String uri) throws IOException {
         org.apache.http.client.HttpClient client = HttpClientBuilder.create ( ).build ( );
         HttpGet request = new HttpGet (this.baseUri + uri);
+        request.setHeader ("X-Access-Token", accessToken);
         HttpResponse response = client.execute (request);
-        BufferedReader reader = new BufferedReader (new InputStreamReader (response.getEntity ( ).getContent ( )));
-        String text = readStreamFromReader (reader);
-        logger.info (text);
+        if (response.getStatusLine ( ).getStatusCode ( ) == 200) {
+            BufferedReader reader = new BufferedReader (new InputStreamReader (response.getEntity ( ).getContent ( )));
+            String text = readStreamFromReader (reader);
+            String contentType = response.getEntity ( ).getContentType ( ).getValue ( );
+            if (contentType.equals ("application/json")) {
+                logger.info ("ContentType : " + contentType);
+                searchJsonKey ("link", JsonParser.parseString (text));
+            }
+            logger.info ("Content Was Received \n" + text);
+        } else {
+            logger.error ("Http Request Failed with status code : " + response.getStatusLine ( ).getStatusCode ( ));
+        }
+    }
+
+    private void searchJsonKey(String key, JsonElement jsonElement) {
+
+        if (jsonElement.isJsonArray ( )) {
+            for (JsonElement jsonElement1 : jsonElement.getAsJsonArray ( )) {
+                searchJsonKey (key, jsonElement1);
+            }
+        } else {
+            if (jsonElement.isJsonObject ( )) {
+                Set <Map.Entry <String, JsonElement>> entrySet = jsonElement
+                        .getAsJsonObject ( ).entrySet ( );
+                for (Map.Entry <String, JsonElement> entry : entrySet) {
+                    String key1 = entry.getKey ( );
+                    if (key1.equals (key)) {
+                        String value = entry.getValue ( ).toString ( );
+                        try {
+                            JSONObject jsonKeyObj = new JSONObject (value);
+                            Iterator <String> keySet = jsonKeyObj.keys ( );
+                            while (keySet.hasNext ( )) {
+                                list.add (jsonKeyObj.get (keySet.next ( )).toString ( ));
+                            }
+                        } catch (JSONException e) {
+                            logger.warn (e.getMessage ( ));
+                            list.add (value.replace ("\"", ""));
+                        }
+
+                    }
+                    searchJsonKey (key, entry.getValue ( ));
+                }
+            } else {
+                if (jsonElement.toString ( ).equals (key)) {
+                    list.add (jsonElement.toString ( ));
+                }
+            }
+        }
 
 
     }
